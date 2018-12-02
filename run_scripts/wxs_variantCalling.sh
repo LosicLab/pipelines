ab3466d29e9ab34e219e33664f02ae8d21942392
Paula Restrepo <paularstrpo@gmail.com>
2018-12-02 17:19:59 -0500

    add run scripts for wxs pipeline

---
+++ run_scripts/wxs_variantCalling.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# The purpose of this script is to run the LosicLab's custom fork of nextflow's wholeexome sequencing processing pipeline for fastq->vcf.
+
+
+# specify account for lsf runs
+account='acc_JanssenIBD' # do not leave this to default unless running interactive profile
+job_queue='premium' #default queue is alloc
+
+# Run directory. Pipeline outputs to $rundir/Processed
+rundir='/sc/orga/projects/losicb01a/common_folder/nextflow-pipelines/wxs-test'
+
+# Reference genome to use [Default: GRCh38; other refs not yet supported]
+ref="GRCh38"
+
+# Path to directory containing the pipeline to run
+pipeline='/sc/orga/projects/losicb01a/common_folder/nextflow-pipelines/exoseq'
+mkdir -p $rundir
+cd $rundir
+
+inputfq="/sc/orga/projects/losicb01a/common_folder/nextflow-pipelines/test-datasets/testdata/*{R1,R2}*.fastq.gz"
+
+module load nextflow/0.30.2
+#NXF_OPTS='-Xms1g'
+
+#while read line ; do
+
+#    sampleID=${line##/sc*/}
+#    echo $sampleID
+
+nextflow run $pipeline/preprocessing.nf \
+--outdir $rundir/preprocessing \
+--reads "$inputfq" \
+--genome $ref \
+--saveAlignedIntermediates true \
+--minerva_account $account \
+--job_queue $job_queue \
+-resume \
+-profile standard
+
+
+#done<list
